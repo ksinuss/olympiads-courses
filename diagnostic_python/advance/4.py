d58f22d689e15b0f4985afe070179db7f24f7372
ksinuss <kostionovaki@gmail.com>
2024-06-14 22:52:04 +0300

    4_diagnostic_python

--- diagnostic_python/advance/4.py
+++ diagnostic_python/advance/4.py
@@ -15,115 +15,76 @@ def get_csv_file(output: list):
         writer.writeheader()
         writer.writerows(output)
 
-def form_output(
-        elements: dict = {},
-        output: dict = {
-            'expedition_id': 0, 
-            'designation': '', 
-            'target': '', 
-            'ship': '', 
-            'engine': '', 
-            'year': 0
-        }
-):
+def form_output(elements: dict, filler: dict):
     output_ = output
     for heading in elements:
         output_[heading] = elements[heading]
-    if all(output[heading] for heading in output_):
-        OUTPUT.append(output_)
-    return output
+        print(output_)
+        if all(output[heading] for heading in output_):
+            OUTPUT.append(output_)
+        return output
 
-def choosing_next_step(name_table, sign, correct_id: list, output):
+def choosing_next_step(name_table, sign, correct_id: list):
     print(name_table, sign, correct_id)
-    if name_table == 'engines':
-        if sign == '<':
-            for id in correct_id:
-                engine_id = id[0]
-                engine = id[1]
-                output_ = form_output({'engine': engine})
-                print(output_)
-                take_correct_id('ships', 'engine_id', '=', engine_id, output_)
-        if sign == '=':
-            pass
-    if name_table == 'ships':
-        if sign == '<':
-            pass
-        if sign == '=':
-            for id in correct_id:
-                ship_id = id[0]
-                ship = id[1]
-                output_ = form_output({'ship': ship}, output)
-                print(output_)
-                take_correct_id('expeditions', 'ship_id', '=', ship_id, output_)
-    if name_table == 'expeditions':
-        if sign == '<':
-            pass
-        if sign == '=':
-            for id in correct_id:
-                form_output(
-                    {
-                        'expedition_id': id[0],
-                        'designation': id[1],
-                        'target': id[2],
-                        'year': id[3]
-                    },
-                    output
-                )
+    
 
-def take_correct_id(name_table, name_column, sign, value, output={}):
+    # if name_table == 'engines':
+    #     if sign == '<':
+    #         for id in correct_id:
+    #             engine_id = id[0]
+    #             engine = id[1]
+    #             output_ = form_output({'engine': engine})
+    #             # print(output_)
+    #             take_correct_id('ships', 'engine_id', '=', engine_id, output_)
+    #     if sign == '=':
+    #         pass
+    # if name_table == 'ships':
+    #     if sign == '<':
+    #         pass
+    #     if sign == '=':
+    #         for id in correct_id:
+    #             ship_id = id[0]
+    #             ship = id[1]
+    #             output_ = form_output({'ship': ship}, output)
+    #             print(output_)
+    #             take_correct_id('expeditions', 'ship_id', '=', ship_id, output_)
+    # if name_table == 'expeditions':
+    #     if sign == '<':
+    #         pass
+    #     if sign == '=':
+    #         for id in correct_id:
+    #             form_output(
+    #                 {
+    #                     'expedition_id': id[0],
+    #                     'designation': id[1],
+    #                     'target': id[2],
+    #                     'year': id[3]
+    #                 },
+    #                 output
+    #             )
+
+def take_correct_id(name_table, name_column, sign, value: list):
     try:
         with sqlite3.connect(NAME_FILE) as conn:
             cursor = conn.cursor()
-            request_initial_information = f'''
-                SELECT ALL
-                FROM {name_table} 
-                WHERE {name_column} <= "{value}"
-            '''
-            request_add_selection = f'''
-                SELECT ALL
-                FROM {name_table}
-                WHERE {name_column} == {value}
-            '''
-            request = f'''
-                SELECT *
-                FROM {name_table} 
-                WHERE {name_column} {sign}= {value}
-            '''
+            if sign == '<':
+                value = value[0]
+                request = f'''
+                    SELECT *
+                    FROM {name_table} 
+                    WHERE {name_column} <= "{value}"
+                '''
+            if sign == '=':
+                marks = ','.join(value)
+                request = f'''
+                    SELECT *
+                    FROM {name_table}
+                    WHERE {name_column} IN ({marks})
+                '''
             cursor.execute(request)
             correct_id = cursor.fetchall() # fetchone - ТОЛЬКО 1 запись, fetchall - ВСЕ записи
             print(correct_id)
-            choosing_next_step(name_table, sign, correct_id, output)
-            variations = {
-                'expeditions': {
-                    'designation': {
-                        'name_table': 'ships',
-                        'name_column': 'id',
-                        'sign': '=',
-                        'value': 4
-                    },
-                    'target': {
-                        'name_table': 'ships',
-                        'name_column': 'id',
-                        'sign': '=',
-                        'value': 4
-                    },
-                    'year': {
-                        'name_table': 'ships',
-                        'name_column': 'id',
-                        'sign': '=',
-                        'value': 4
-                    },
-                    'ship_id': form_output
-                },
-                'ships': {
-                    'type': {
-                        'name_table': 'ships',
-                        'name_column': 'id',
-                        'sign': '=',
-                        'value': 4
-                    }
-                }   
-            }
+            choosing_next_step(name_table, sign, correct_id)
             # return correct_id
     except Exception as e:
         print(e)
@@ -137,9 +98,48 @@ NAME_FILE = 'expeditions.db'
 name_table, name_column = 'engines', 'engine'
 max_value = 'electric'
 
+steps = {
+    'expeditions': {
+        '<': {
+            'name_table': 'ships',
+            'name_column': 'id',
+            'sign': '='
+        },
+        '=': {
+            'name_table': 'engines',
+            'name_column': 'id',
+            'sign': '='
+        }
+    },
+    'ships': {
+        '<': {
+            'name_table': 'expeditions',
+            'name_column': 'ship_id',
+            'sign': '='
+        },
+        '=': {
+            'name_table': 'expeditions',
+            'name_column': 'ship_id',
+            'sign': '='
+        }
+    },
+    'engines': {
+        '<': {
+            'name_table': 'ships',
+            'name_column': 'engine_id',
+            'sign': '='
+        },
+        '=': {
+            'name_table': 'ships',
+            'name_column': 'id',
+            'sign': '=',
+            'value': 4
+        }
+    }
+}
 OUTPUT = []
 
-take_correct_id(name_table, name_column, '<', f'"{max_value}"')
+take_correct_id(name_table, name_column, '<', [max_value])
 
 print(OUTPUT)
-get_csv_file(OUTPUT)
+# get_csv_file(OUTPUT)
